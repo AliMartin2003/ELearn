4b166afa4c1d4762c57d1e32fdd366aa6994e320
HighEnd <HighEnd@DESKTOP-K2V8996>
2025-04-16 11:32:47 +0330

    Add ICourse Interface

---
+++ ELearn.Core/Interfaces/ICourse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELearn.Core.Interfaces
+{
+    internal interface ICourse
+    {
+    }
+}
