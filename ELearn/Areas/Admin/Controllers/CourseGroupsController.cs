c57bab3a6ce770f0cb0e63eacd5d9b57c3eff557
HighEnd <HighEnd@DESKTOP-K2V8996>
2025-04-17 12:18:27 +0330

    asdasdadasAdd Create CourseGroup Proccess

--- ELearn/Areas/Admin/Controllers/CourseGroupsController.cs
+++ ELearn/Areas/Admin/Controllers/CourseGroupsController.cs
@@ -1,4 +1,5 @@
 ﻿using ELearn.Core.Interfaces;
+using ELearn.DataLayer.DTOS.CourseGroupViewModels;
 using ELearn.DataLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,6 +19,83 @@ namespace ELearn.Areas.Admin.Controllers
         {
             List<CourseGroup> courseGroups = _courseGroupServices.GetCourseGroups().ToList();
             return View(courseGroups);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCourseGroup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCourseGroup(CreateCourseGroupViewModel createModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(createModel);
+            }
+            CourseGroup courseGroup = new CourseGroup
+            {
+                CourseGroupName = createModel.CourseGroupName,
+
+            };
+            if (!await _courseGroupServices.CreateCourseGroup(courseGroup))
+            {
+                TempData["Error"] = "عملیات با موفقیت شکست خورد !";
+                return Redirect("/Admin/CourseGroups/index");
+            }
+            TempData["Success"] = "عملیات با موفقیت پیروز شد !";
+            return Redirect("/Admin/CourseGroups/index");
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditCourseGroup(int id)
+        {
+            CourseGroup courseGroup = await _courseGroupServices.GetCourseGroupById(id);
+            if (courseGroup == null)
+            {
+                TempData["Error"] = "گروه یافت نشد";
+                return Redirect("/admin/CourseGroups");
+            }
+            EditCourseGroupViewModel edit = new EditCourseGroupViewModel
+            {
+                CourseGroupName = courseGroup.CourseGroupName,
+                Id = id
+            };
+            return View(edit);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditCourseGroup(EditCourseGroupViewModel edit)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(edit);
+            }
+            CourseGroup courseGroup = new CourseGroup
+            {
+                CourseGroupId = edit.Id,
+                CourseGroupName = edit.CourseGroupName,
+            };
+            if ( !await _courseGroupServices.UpdateCourseGroup(courseGroup))
+            {
+                TempData["Error"] = "عملیات با موفقیت شکست خورد";
+                return Redirect("/Admin/CourseGroups/index");
+            }
+            TempData["Success"] = "عملیات با موفقیت انجام شد";
+            return Redirect("/Admin/CourseGroups/index");
+        }
+
+        public async Task<IActionResult> DeleteCourseGroup(int id)
+		{
+			if (!await _courseGroupServices.DeleteCourseGroup(id))
+			{
+               
+                    TempData["Error"] = "عملیات با موفقیت شکست خورد";
+                    return Redirect("/Admin/CourseGroups/index");
+            }
+            TempData["Success"] = "عملیات با موفقیت انجام شد";
+            return Redirect("/Admin/CourseGroups/index");
         }
     }
 }
