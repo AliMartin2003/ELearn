8876370c5de4ce1f9f126842e46a899888d3b64f
HighEnd <HighEnd@DESKTOP-K2V8996>
2025-04-17 12:18:27 +0330

    Add Create CourseGroup Proccess

--- ELearn/Areas/Admin/Controllers/CourseGroupsController.cs
+++ ELearn/Areas/Admin/Controllers/CourseGroupsController.cs
@@ -1,4 +1,5 @@
 ﻿using ELearn.Core.Interfaces;
+using ELearn.DataLayer.DTOS.CourseGroupViewModels;
 using ELearn.DataLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,6 +19,34 @@ namespace ELearn.Areas.Admin.Controllers
         {
             List<CourseGroup> courseGroups = _courseGroupServices.GetCourseGroups().ToList();
             return View(courseGroups);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCourseGroup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCourseGroup(CreateCourseGroupViewModel createModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(createModel);
+            }
+            CourseGroup courseGroup = new CourseGroup
+            {
+                CourseGroupName = createModel.CourseGroupName,
+
+            };
+            if (!await _courseGroupServices.CreateCourseGroup(courseGroup))
+            {
+                TempData["Error"] = "عملیات با موفقیت شکست خورد !";
+                return RedirectToAction("index");
+            }
+            TempData["Success"] = "عملیات با موفقیت پیروز شد !";
+            return RedirectToAction("index");
+
         }
     }
 }
