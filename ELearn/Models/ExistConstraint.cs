0956d6f8921dbf6019d6ed4742945dcacc1d9da4
HighEnd <HighEnd@DESKTOP-K2V8996>
2025-05-07 12:08:18 +0330

    new Template

---
+++ ELearn/Models/ExistConstraint.cs
@@ -0,0 +1,10 @@
+﻿//namespace ELearn.Models
+//{
+//    public class ExistConstraint : IRouteConstraint
+//    {
+//        public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+//        {
+//            return values.ContainsKey(routeKey);
+//        }
+//    }
+//}
